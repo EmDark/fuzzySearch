d644fc4e196f7ea2a0099bb6fedd5a3e0149be47
huliang46396 <huliang46396@hundsun.com>
2023-07-21 15:04:22 +0800

    Initial commit

---
+++ src/test/java/com/leon/fuzzysearch/FuzzySearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leon.fuzzysearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FuzzySearchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
